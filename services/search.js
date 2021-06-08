ba946633a2d37419d5f1f22e476ee110e1a9cb42
abhishek-geek <2019005@iiitdmj.ac.in>
2021-06-08 16:37:50 +0530

    initialised docker

--- services/search.js
+++ services/search.js
@@ -1,24 +1,49 @@
 const { google } = require("googleapis");
 const { YOUTUBE_TOKEN } = require("../utils/config");
 
+// Trying different API keys
+const tokenArray = YOUTUBE_TOKEN.split(",");
+
+let currentTokenIndex = 0;
+
 // this function will return new videos ater the provided date and time (after)
 // with the search query "vlog"
-
 const getNewVideos = async (after) => {
-  console.log("after || latestDate", after);
-  const response = await google.youtube("v3").search.list({
-    key: YOUTUBE_TOKEN,
-    part: "snippet",
-    q: "vlog",
-    publishedAfter: after,
-    // maxResults: 5,
-  });
+  console.log("currentToken", tokenArray[currentTokenIndex]);
 
-  console.log("YEP....");
-  const { data } = response;
-  const details = data.items.map((item) => item.snippet);
-  console.log(details);
-  return details;
+  try {
+    const response = await google.youtube("v3").search.list({
+      key: tokenArray[currentTokenIndex],
+      part: "snippet",
+      q: "vlog",
+      publishedAfter: after,
+      maxResults: 1,
+    });
+    console.log("in line 22");
+    console.log("response from yt : ", response);
+    console.log("YEP....");
+    const { data } = response;
+    const details = data.items.map((item) => item.snippet);
+    console.log("details : ", details);
+    return details;
+  } catch (err) {
+    console.error(err);
+    currentTokenIndex = currentTokenIndex + 1;
+    getNewVideos(after);
+  }
+  // const response = await google.youtube("v3").search.list({
+  //   key: YOUTUBE_TOKEN,
+  //   part: "snippet",
+  //   q: "vlog",
+  //   publishedAfter: after,
+  //   // maxResults: 5,
+  // });
+
+  // console.log("YEP....");
+  // const { data } = response;
+  // const details = data.items.map((item) => item.snippet);
+  // console.log(details);
+  // return details;
 };
 
 // getNewVideos("2021-06-04T15:50:55Z");
